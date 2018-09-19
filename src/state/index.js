df22584fa388ebd126e8157b7971a81d57339dfb
Joshua Seigler <joshua@seigler.net>
2018-09-19 18:00:37 -0400

    :zap: DPW-229 Create currency preference in app data

--- src/state/index.js
+++ src/state/index.js
@@ -6,10 +6,13 @@
 import { combineReducers } from 'redux';
 import { createStore } from 'redux';
 import language from './settings/language';
+import currency from './settings/currency';
 export * from './settings/language';
+export * from './settings/currency';
 
 const rootReducer = combineReducers({
   settings: combineReducers({
+    currency: currency,
     language: language
   })
 });
--- src/state/settings/currency/index.js
+++ src/state/settings/currency/index.js
@@ -3,3 +3,24 @@
  *
  * @flow
  */
+
+const CHANGE_CURRENCY = 'CHANGE_CURRENCY';
+const initialState = 'usd';
+
+const currency = (state = initialState, action) => {
+  switch (action.type) {
+    case CHANGE_CURRENCY:
+      return action.currency;
+
+    default:
+      return state;
+  }
+};
+export default currency;
+
+export const changeCurrency = currency => ({
+  type: CHANGE_CURRENCY,
+  currency
+});
+
+export const selectCurrency = state => state.settings.currency;
