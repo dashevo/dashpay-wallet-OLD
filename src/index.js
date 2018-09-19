7439cd916cb6dfd01cc6ba6f938e21b84b4fb17c
Denis Stepanov <iamstdenis@gmail.com>
2018-09-19 19:58:10 +0800

    react-native-navigator linked with ios

--- src/index.js
+++ src/index.js
@@ -18,50 +18,52 @@ import { COLORS } from 'constants';
 import state from 'state';
 
 function app() {
-  // TODO: asynchronously
-  Navigation.registerComponent('SplashScreen', () => SplashScreen);
-  Navigation.registerComponent('HomeScreen', () => HomeScreen);
-  Navigation.registerComponent('SendScreen', () => SendScreen);
-  Navigation.registerComponent('ReceiveScreen', () => ReceiveScreen);
-  Navigation.registerComponent('ContactsScreen', () => ContactsScreen);
-  Navigation.registerComponentWithRedux(
-    'SettingsScreen',
-    () => SettingsScreen,
-    StoreProvider,
-    state
-  );
-  Navigation.setRoot({
-    root: {
-      stack: {
-        id: 'DashPay',
-        options: {
-          statusBar: {
-            style: 'light',
-            backgroundColor: COLORS.blue
-          },
-          layout: {
-            orientation: ['portrait'],
-            backgroundColor: COLORS.gray
-          },
-          topBar: {
-            visible: false,
-            animate: false,
-            hideOnScroll: false,
-            drawBehind: false,
-            background: {
-              color: COLORS.bluedim
+  Navigation.events().registerAppLaunchedListener(() => {
+    // TODO: asynchronously
+    Navigation.registerComponent('SplashScreen', () => SplashScreen);
+    Navigation.registerComponent('HomeScreen', () => HomeScreen);
+    Navigation.registerComponent('SendScreen', () => SendScreen);
+    Navigation.registerComponent('ReceiveScreen', () => ReceiveScreen);
+    Navigation.registerComponent('ContactsScreen', () => ContactsScreen);
+    Navigation.registerComponentWithRedux(
+      'SettingsScreen',
+      () => SettingsScreen,
+      StoreProvider,
+      state
+    );
+    Navigation.setRoot({
+      root: {
+        stack: {
+          id: 'DashPay',
+          options: {
+            statusBar: {
+              style: 'light',
+              backgroundColor: COLORS.blue
+            },
+            layout: {
+              orientation: ['portrait'],
+              backgroundColor: COLORS.gray
+            },
+            topBar: {
+              visible: false,
+              animate: false,
+              hideOnScroll: false,
+              drawBehind: false,
+              background: {
+                color: COLORS.bluedim
+              }
             }
-          }
-        },
-        children: [
-          {
-            component: {
-              name: 'SplashScreen'
+          },
+          children: [
+            {
+              component: {
+                name: 'SplashScreen'
+              }
             }
-          }
-        ]
+          ]
+        }
       }
-    }
+    });
   });
 }
 
