e10fd9d2ef9e98cc30298734e4616c0a8e5efcf1
Kreshnik Alidema <kreshnik.alidema@gmail.com>
2018-10-15 13:43:59 +0200

    Redux action - initialize wallet lib.

--- src/state/index.js
+++ src/state/index.js
@@ -3,14 +3,15 @@
  *
  * @wolf
  */
-import { createStore } from 'redux';
-import reducers from './reducers';
+import { createStore } from "redux";
+import reducers from "./reducers";
 
 // Tmp
-export * from './settings';
-export * from './language';
-export * from './currency';
-export * from './user';
+export * from "./actions";
+export * from "./settings";
+export * from "./language";
+export * from "./currency";
+export * from "./user";
 
 // Tmp
 const walletLib = {
@@ -24,17 +25,17 @@ const walletLib = {
         mnemonic
       });
       walletLib.account = this.wallet.getAccount(0);
-      let listener = walletLib.account.events.on('ready', () => {
+      let listener = walletLib.account.events.on("ready", () => {
         resolve(true);
       });
     });
   }
 };
 
-import thunk from 'redux-thunk';
-import middleware from './middleware';
-import { applyMiddleware } from 'redux';
-import { Wallet } from '@dashevo/wallet-lib';
+import thunk from "redux-thunk";
+import middleware from "./middleware";
+import { applyMiddleware } from "redux";
+import { Wallet } from "@dashevo/wallet-lib";
 
 const extraArgument = thunk.withExtraArgument(walletLib);
 const enhancedMiddleware = applyMiddleware(middleware, extraArgument);
--- src/state/user/reducer.js
+++ src/state/user/reducer.js
@@ -3,7 +3,12 @@
  *
  * @wolf
  */
-export const initialState = {};
+export const initialState = {
+  network: "testnet", //todo : Should be from state
+  mnemonic:
+    "differ beach latin proof gorilla aisle apple brain goddess crash dolphin wine",
+  transport: "insight"
+};
 
 const user = (state = initialState, action) => {
   switch (action.type) {
