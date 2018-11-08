e42faf60bf7b43882037d7180b8f406b46f7d5b7
Alex Werner <alex@werner.fr>
2018-11-08 21:34:08 +0100

    Delete : Wallet-lib unused code

--- src/enhancer.js
+++ src/enhancer.js
@@ -11,55 +11,12 @@ import state from "state";
 import translations from "translations";
 import type { ReactComponent } from "./types";
 import type { ReactElement } from "./types";
-import { Wallet } from "@dashevo/wallet-lib";
-
-const walletLib = {
-  wallet: null,
-  account: null,
-  async payTo(payType, recipient, amount, isIS = true) {
-    const txOpts = {
-      amount: parseFloat(amount),
-      to: recipient,
-      isInstantSend: isIS
-    };
-    return walletLib.account.broadcastTransaction(
-      walletLib.account.createTransaction(txOpts),
-      txOpts.isInstantSend
-    );
-  },
-  initializeWallet(opts) {
-    const { network, mnemonic } = opts;
-    return new Promise(resolve => {
-      walletLib.wallet = new Wallet({
-        network,
-        mnemonic,
-        transport: "Insight"
-      });
-      walletLib.account = this.wallet.getAccount(0);
-      walletLib.account.events.on("ready", () => {
-        resolve(true);
-      });
-    });
-  },
-  /**
-   * When using the walletLib wrapper, we call our method from walletLib.account.
-   * This method allow to update the account reference used in the app.
-   * @param accountId - A positive numeric value
-   */
-  changeAccount(accountId = 0) {
-    walletLib.account = this.wallet.getAccount(accountId);
-  },
-  isAddress(input) {
-    return input.length === 34; //FIXME : This only work for demo purpose but technically wrong
-  }
-};
 
 function enhance(Component: ReactComponent): ReactComponent {
   return class EnhancedComponent extends React.Component<any> {
     constructor(props) {
       super(props);
       this.navigation = navigation(props);
-      this.walletLib = walletLib;
     }
 
     render(): ReactElement {
