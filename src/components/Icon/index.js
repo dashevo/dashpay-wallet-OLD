818615647d09feef0e816eb4f3792efc1db37457
Kreshnik Alidema <kreshnik.alidema@gmail.com>
2018-11-09 12:24:51 +0100

    Icon must be consistent with other components.

---
+++ src/components/Icon/index.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2014-present, Dash Core Group, Inc.
+ *
+ * @flow
+ */
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import DashPayFontIcons from 'assets/fonts/config.json';
+
+const Icon = createIconSetFromIcoMoon(DashPayFontIcons);
+
+export default Icon;
