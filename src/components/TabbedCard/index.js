efeb7eb3aa8d3ae2c85881bfa075ee3d77782df0
Joshua Seigler <joshua@seigler.net>
2018-11-19 22:08:57 -0500

    :construction: Partial work, will be amended

--- src/components/TabbedCard/index.js
+++ src/components/TabbedCard/index.js
@@ -15,6 +15,7 @@ import {
   ScrollView,
   TouchableHighlight,
   TouchableWithoutFeedback,
+  Animated,
 } from 'react-native';
 import { connect } from 'react-redux';
 import ScrollableTabView from 'react-native-scrollable-tab-view';
@@ -28,23 +29,32 @@ class TabbedCard extends React.Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
-      fakeText: Array.from({length: 1000},
-        () => Math.random().toString(36).substring(2+Math.floor(Math.random()*6)) + (Math.random()>0.2?' ':'. ')
-      )
+      fakeText: Array.from({length: 1000},()=>Math.random().toString(36).substr(2+Math.pow(Math.random(),.2)*8).replace(/\d/g,'eai'.charAt(Math.random()*5))+(Math.random()>0.2?' ':'. ')).join('').replace(/.+?[\.\?\!](\s|$)/g,(t)=>t.charAt(0).toUpperCase()+t.substr(1).toLowerCase()),
+      scrollOffset: new Animated.Value(0),
     };
     // this.onSettingsPressed = this.onSettingsPressed.bind(this);
     // this.animation = React.createRef();
   }
 
+  onScroll = e => {
+    this.state.scrollOffset.setValue(e.nativeEvent.contentOffset.y);
+  };
+
   render() {
     return (
       <View style={styles.card}>
         <View style={styles.header}>
-          <Avatar source={require('assets/images/avatar-default.png')} />
+          <Animated.View style={styles.disappearingHeaderPart}>
+            <Avatar source={require('assets/images/avatar-default.png')} />
+          </Animated.View>
           <Text style={styles.cardTitle}>Contact Name</Text>
         </View>
         <ScrollableTabView
-          renderTabBar={() => <CustomTabBar />}
+          renderTabBar={
+            () => <CustomTabBar
+              scrollOffset={this.state.scrollOffset}
+            />
+          }
           initialPage={0}
         >
           <ScrollView style={styles.dummyPage} tabLabel='One (tall)'>
