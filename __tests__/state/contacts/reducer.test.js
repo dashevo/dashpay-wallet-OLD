efb4589e84874b4eec6bf75c35fa1112367e56eb
Denis <st.denis@me.com>
2018-12-04 18:24:40 +0800

    more actions and reducers

--- __tests__/state/contacts/reducer.test.js
+++ __tests__/state/contacts/reducer.test.js
@@ -1,6 +1,8 @@
 import { ADD_CONTACT } from 'state/contacts/constants';
 import { CONTACT_REQUEST_SENT } from 'state/contacts/constants';
 import { CONTACT_REQUEST_RECEIVED } from 'state/contacts/constants';
+import { UPDATE_CONTACT } from 'state/contacts/constants';
+import { REMOVE_CONTACT } from 'state/contacts/constants';
 import { CONTACT_STATES } from 'state/contacts/constants';
 import { initialState } from 'state/contacts/reducer';
 import reducer from 'state/contacts/reducer';
@@ -13,7 +15,7 @@ describe('contacts reducer', () => {
   it('should handle ADD_CONTACT', () => {
     const contact = { address: 'contact_address' };
     const payload = { contact, type: ADD_CONTACT };
-    const expectedState = [ ...initialState, { ...contact, state: CONTACT_STATES.ADDED } ];
+    const expectedState = [...initialState, { ...contact, state: CONTACT_STATES.ADDED }];
     expect(reducer(undefined, payload)).toEqual(expectedState);
   });
 
@@ -28,8 +30,25 @@ describe('contacts reducer', () => {
   it('should handle CONTACT_REQUEST_RECEIVED', () => {
     const contact = { address: 'contact_address' };
     const payload = { contact, type: CONTACT_REQUEST_RECEIVED };
-    const expectedState = [ ...initialState, { ...contact, state: CONTACT_STATES.REQUEST_RECEIVED } ];
+    const expectedState = [...initialState, { ...contact, state: CONTACT_STATES.REQUEST_RECEIVED }];
     expect(reducer(undefined, payload)).toEqual(expectedState);
+  });
+
+  it('should handle REMOVE_CONTACT', () => {
+    const address = 'contact_address1';
+    const untouchedContact = { address: 'contact_address2' };
+    const state = [{ address }, untouchedContact];
+    const payload = { address, type: REMOVE_CONTACT };
+    const expectedState = [untouchedContact];
+    expect(reducer(state, payload)).toEqual(expectedState);
+  });
+
+  it('should handle UPDATE_CONTACT', () => {
+    const address = 'contact_address';
+    const state = [{ address, name: 'name1' }];
+    const payload = { contact: { address, name: 'name2' }, type: UPDATE_CONTACT };
+    const expectedState = [{ address, name: 'name2' }];
+    expect(reducer(state, payload)).toEqual(expectedState);
   });
 });
 
--- src/state/contacts/actions.js
+++ src/state/contacts/actions.js
@@ -1,10 +1,22 @@
 import { ADD_CONTACT } from 'constants';
 import { CONTACT_REQUEST_RECEIVED } from 'constants';
 import { CONTACT_REQUEST_SENT } from 'constants';
+import { REMOVE_CONTACT } from 'constants';
+import { UPDATE_CONTACT } from 'constants';
 
 export const addContact = contact => ({
   contact,
   type: ADD_CONTACT,
+});
+
+export const removeContact = address => ({
+  address,
+  type: REMOVE_CONTACT,
+});
+
+export const updateContact = contact => ({
+  contact,
+  type: UPDATE_CONTACT,
 });
 
 export const contactRequestSent = address => ({
