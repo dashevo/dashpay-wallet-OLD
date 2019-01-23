48d6602af5b914f2a64bda7bef76a48d13efc9f4
Joshua Seigler <joshua@seigler.net>
2019-01-22 23:20:43 -0500

    :zap: DPW-491 Improve Avatar component to accept additional styles

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -5,12 +5,27 @@
  */
 import * as React from 'react';
 import { Image } from 'react-native';
-import styles from './styles';
 import type { Props } from './types';
 import type { ReactElement } from './types';
 
-const Avatar = (props: Props): ReactElement => (
-  <Image {...props} style={styles.avatar} />
-);
+const Avatar = (props: Props): ReactElement => {
+  const defaultProps = {
+    style: {
+      height: 100,
+      width: 100,
+      borderRadius: 50
+    },
+    source: require('assets/images/avatar-default.png')
+  };
+
+  const mergedProps = {
+    ...defaultProps,
+    ...props
+  };
+
+  return (
+    <Image source={mergedProps.source} style={mergedProps.style} />
+  );
+};
 
 export default Avatar;
