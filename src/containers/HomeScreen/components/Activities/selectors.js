8f81ceefa7c291e54fbf98d350f523d1979f4ad4
Denis <st.denis@me.com>
2019-04-09 20:40:30 +0800

    Activity sorting imp
    This is a temp fix. As soon as we don't have timestamp in the contact requests yet i set it to now to see them on the top of activity list

--- src/containers/HomeScreen/components/Activities/selectors.js
+++ src/containers/HomeScreen/components/Activities/selectors.js
@@ -4,23 +4,22 @@
  * @flow
  */
 import { createSelector } from 'reselect';
-import { orderBy } from 'lodash';
+import { selectRequests } from 'state/transactions';
 import { selectTransactions } from 'state/transactions';
 
-function mapStateToProps(state, props) {
-  let requests = [...state.contacts.blockchain.requests];
-  let transactions = selectTransactions(state);
+export default createSelector(
+  selectTransactions,
+  selectRequests,
+  (transactions, requests) => {
+    let activity = transactions.map(item => ({type: 'wallet', data: item, time: item.timestamp}));
 
-  let activity = transactions.map(item => ({type: 'wallet', data: item, time: item.timestamp}));
+    activity = activity.concat(
+      requests.map(item => ({type: 'social', data: item, time: new Date}))
+    );
+    activity = activity.sort((a, b) => b.time - a.time);
 
-  activity = activity.concat(
-    requests.map(item => ({type: 'social', data: item, time: 0}))
-  );
-  activity = activity.sort((a,b) => a.time > b.time ? -1 : 1) // descending by time
-
-  return {
-    activity
-  };
-}
-
-export default mapStateToProps;
+    return {
+      activity
+    };
+  }
+);
