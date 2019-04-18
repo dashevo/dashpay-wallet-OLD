c45cb74a3f1863a4ac3c041661d13983b5abae01
Joshua Seigler <joshua@seigler.net>
2019-04-18 17:49:36 -0400

    Move transaction type values into a constants file Also update package.json reference to wallet-lib, no longer using #demo

--- src/state/transactions/selectors.js
+++ src/state/transactions/selectors.js
@@ -5,22 +5,23 @@
  */
 
 import { selectAlternativeCurrency } from 'state/alternativeCurrency';
+import TXTYPES from 'constants/txtypes';
 
 export const selectRequests = state => state.contacts.blockchain.requests;
 
 export const selectTransactions = state => {
   let transactions = [];
   state.transactions.history.forEach(item => {
-    if (item.type == 'moved') {
+    if (item.type == TXTYPES.MOVED) {
       return;
     }
     let dashSat = item.to.reduce((accumulator,item) => (accumulator + item.valueSat), 0); // TODO don't assume we receive every TX output
     let sender = { name: state.account.username, image: null };
     let receiver = { name: state.account.username, image: null };
-    if (item.type == 'receive') { // TODO refer to an imported constant
+    if (item.type == TXTYPES.RECEIVED) { // TODO refer to an imported constant
       const fromAddress = item.from.length > 1 ? 'multiple addresses' : item.from[0].address;
       sender = { name: 'Unidentified Sender', image: null, address: fromAddress };
-    } else if (item.type == 'sent') { // TODO refer to an imported constant
+    } else if (item.type == TXTYPES.SENT) { // TODO refer to an imported constant
       const toAddress = item.to.length > 1 ? 'multiple recipients' : item.to[0].address;
       receiver = { name: 'Unidentified Recipient', image: null, address: toAddress };
     }
