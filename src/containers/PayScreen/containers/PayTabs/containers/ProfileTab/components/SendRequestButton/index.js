b547fab968ffa7d9d3e21a498d1e8c920983d7eb
Joshua Seigler <joshua@seigler.net>
2019-05-23 11:43:42 -0400

    refactor(pay tab): remove unused style
    DPW-699

--- src/containers/PayScreen/containers/PayTabs/containers/ProfileTab/components/SendRequestButton/index.js
+++ src/containers/PayScreen/containers/PayTabs/containers/ProfileTab/components/SendRequestButton/index.js
@@ -5,7 +5,6 @@ import { TouchableOpacity } from 'react-native';
 import Text from 'components/Text';
 import Button from 'components/Button';
 import actions from './actions';
-// import styles from './styles';
 import Types from './types';
 
 const SendRequestButton = ({ address, sendContactRequest }: Types) => (
