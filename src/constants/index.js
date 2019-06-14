55f8eb1b794fe708e48bfee444f234696c21d4dc
Kreshnik Alidema <kreshnik.alidema@gmail.com>
2019-06-14 13:55:49 +0200

    fix(confirmation screen): removes the unused constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,20 +3,16 @@
  *
  * @flow
  */
-import { Dimensions } from 'react-native';
+import { Dimensions } from "react-native";
 
-export { default as COLORS } from './colors';
-export { default as THEMES } from './theming';
-export { default as LENGTHS } from './lengths';
-export { default as ALTERNATIVE_CURRENCIES } from './alternativeCurrencies';
-export { default as TXTYPES } from './txtypes';
+export { default as COLORS } from "./colors";
+export { default as THEMES } from "./theming";
+export { default as LENGTHS } from "./lengths";
+export { default as ALTERNATIVE_CURRENCIES } from "./alternativeCurrencies";
+export { default as TXTYPES } from "./txtypes";
 
-export * from './defaults';
+export * from "./defaults";
 
-// TMP
-export const CURRENCIES = {
-  DASH: { code: 'DASH', name: 'DASH' },
-  USD: { code: 'USD', name: 'US Dollar' },
-};
-
-export const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get('window');
+export const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get(
+  "window"
+);
