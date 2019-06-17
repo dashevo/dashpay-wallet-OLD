e5131a2751505fd22560a25ee4b9f9b379f50ea6
Kreshnik Alidema <kreshnik.alidema@gmail.com>
2019-06-17 12:43:28 +0200

    fix(profile picture): removes the ability to edit the picture (#127)
    * fix(profile picture): removes able to edit the picture
    
    * fix(profile picture): makes eslint happy
    
    * fix(profile picture): rebase against master

--- src/containers/PayScreen/containers/PayTabs/containers/ProfileTab/components/ImageField/index.js
+++ src/containers/PayScreen/containers/PayTabs/containers/ProfileTab/components/ImageField/index.js
@@ -1,6 +1,5 @@
 // @flow
 import * as React from 'react';
-import Touchable from 'components/Touchable';
 import Field from 'components/Field';
 import Avatar from 'components/Avatar';
 import View from 'components/View';
@@ -12,37 +11,17 @@ function ImageField(props) {
       {({ form }) => {
         const { name, image } = form.values;
 
-        function onPress() {
-          form.navigation.navigate('CameraRollScreen', {
-            onSelect: selected => form.setFieldValue('image', selected.selected[0].uri),
-          });
-        }
-
         return (
           <Avatar name={name} image={image} {...props}>
-            {({ children, bind, styles }) => (
-              <Touchable onPress={onPress}>
-                <View style={styles.col}>
-                  <View style={styles.center}>
-                    <View style={styles.container}>
-                      <View style={styles.body}>{children}</View>
-                      <View style={styles.left}>
-                        <Touchable
-                          {...bind}
-                          hitSlop={{
-                            bottom: 10,
-                            left: 10,
-                            right: 10,
-                            top: 10,
-                          }}
-                        >
-                          <Icon style={styles.icon} name="dash" />
-                        </Touchable>
-                      </View>
-                    </View>
+            {({ children, styles }) => (
+              <View style={styles.center}>
+                <View style={styles.container}>
+                  <View style={styles.body}>{children}</View>
+                  <View style={styles.left}>
+                    <Icon style={styles.icon} name="dash" />
                   </View>
                 </View>
-              </Touchable>
+              </View>
             )}
           </Avatar>
         );
