da4163e9563c9598d29f97d4e2873d7f05f65548
Kreshnik Alidema <kreshnik.alidema@gmail.com>
2019-06-17 14:17:55 +0200

    fix(profile picture): tries to make eslint happy one more time

--- src/hooks/Image/index.js
+++ src/hooks/Image/index.js
@@ -1,6 +1,6 @@
-import { Image } from 'react-native';
+import { Image as ImageTmp } from 'react-native';
 
 export { default as Image } from 'react-native-fast-image';
 export { default as useImage } from './useImage';
 
-export default Image;
+export default ImageTmp;
