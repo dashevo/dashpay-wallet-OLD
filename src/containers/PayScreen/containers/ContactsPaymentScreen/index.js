c1d141420170098036e54cdbecfeb860ee4828d7
Alex Werner <alex@werner.fr>
2019-08-06 15:49:28 +0200

    feat(contactspay): styling

--- src/containers/PayScreen/containers/ContactsPaymentScreen/index.js
+++ src/containers/PayScreen/containers/ContactsPaymentScreen/index.js
@@ -4,17 +4,17 @@ import { connect } from 'react-redux';
 import ScrollView from 'components/ScrollView';
 import View from 'components/View';
 import SearchBox from 'components/SearchBox';
-// import ActivityIndicatorView from 'hooks/ActivityIndicatorView';
+import ActivityIndicatorView from 'hooks/ActivityIndicatorView';
 
 import ListEmpty from 'hooks/ContactList/ListEmpty';
 
 import { Animated } from 'react-native';
-// import { Text } from 'react-native-svg';
 import selector from './selectors';
-import useStyles from './useStyles';
+import styles from './styles';
 import type { Props, State } from '../../../ContactsScreen/types';
 import actions from './actions';
 import { Item, ListHeader, ListFooter } from '../../../ContactsScreen/components';
+
 
 class ContactsPaymentTab extends React.Component<Props, State> {
   constructor(props: Props) {
@@ -31,16 +31,16 @@ class ContactsPaymentTab extends React.Component<Props, State> {
       [{ nativeEvent: { contentOffset: { y: this.scrollPos } } }],
       { useNativeDriver: true },
     );
-    // this.state = {
-    //   isSearching: false,
-    //   isFetchingContacts: true,
-    // };
+    this.state = {
+      isSearching: false,
+      isFetchingContacts: true,
+    };
   }
 
   componentDidMount() {
-    // const { getContacts } = this.props;
-    // getContacts()
-    //   .finally(() => this.setState({ isFetchingContacts: false }));
+    const { getContacts } = this.props;
+    getContacts()
+      .finally(() => this.setState({ isFetchingContacts: false }));
   }
 
   componentWillUnmount() {
@@ -62,27 +62,9 @@ class ContactsPaymentTab extends React.Component<Props, State> {
     return <ListEmpty {...this.props} />;
   }
 
-
-  renderMostFrequentContactsBody() {
-    const styles = useStyles();
-    const { sections } = this.props;
-    return (
-      <Animated.SectionList
-        contentContainerStyle={styles.contentContainer}
-        renderSectionHeader={this.renderListHeader}
-        ListFooterComponent={this.renderListFooter}
-        ListEmptyComponent={this.renderListEmpty}
-        keyExtractor={({ username }) => username}
-        sections={sections}
-        renderItem={this.renderItem}
-        onScroll={this.scrollSinkY}
-      />
-    );
-  }
-
   renderContactsBody() {
-    const styles = useStyles();
     const { sections } = this.props;
+    console.log(sections);
     return (
       <Animated.SectionList
         contentContainerStyle={styles.contentContainer}
@@ -98,55 +80,55 @@ class ContactsPaymentTab extends React.Component<Props, State> {
   }
 
   render() {
-    const styles = useStyles();
     const searchBoxStyles = {
-      paddingLeft: 16,
-      paddingRight: 32,
-      backgroundColor: '#000000',
+      marginTop: 16,
+      marginBottom: 16,
+      marginRight: 16,
+      marginLeft: 28,
+      backgroundColor: '#ffffff',
+      borderRadius: 8,
+      borderWidth: 1,
+      borderColor: '#d9d9d9',
+      borderStyle: 'solid',
     };
-    // const animatedStyle = {
-    //   transform: [
-    //     {
-    //       translateY: this.scrollPos.interpolate({
-    //         inputRange: [0, 150],
-    //         outputRange: [0, -150],
-    //         extrapolateRight: 'clamp',
-    //         extrapolateLeft: 'clamp',
-    //       }),
-    //     },
-    //   ],
-    // };
+    const animatedStyle = {
+      transform: [
+        {
+          translateY: this.scrollPos.interpolate({
+            inputRange: [0, 150],
+            outputRange: [0, -150],
+            extrapolateRight: 'clamp',
+            extrapolateLeft: 'clamp',
+          }),
+        },
+      ],
+    };
     // FIXME : Hooks can only be called inside the body of a function component error
-    // const checkIsActiveRequest = () => {
-    //   const { isSearching, isFetchingContacts } = this.state;
-    //   const { sections } = this.props;
-    //   const isActiveRequest = isSearching || isFetchingContacts;
-    //   if (isActiveRequest && sections.length === 0) {
-    //     return true;
-    //   }
-    // };
-    // if (checkIsActiveRequest()) {
-    //   return <ActivityIndicatorView />;
-    // }
+    const checkIsActiveRequest = () => {
+      const { isSearching, isFetchingContacts } = this.state;
+      const { sections } = this.props;
+      const isActiveRequest = isSearching || isFetchingContacts;
+      return isActiveRequest && sections.length === 0;
+    };
+    if (checkIsActiveRequest()) {
+      return <ActivityIndicatorView />;
+    }
     return (
-      <ScrollView style={styles.container}>
-        <View style={styles.row}>
-          <SearchBox
-            placeholder="Search by username or alias"
-            style={searchBoxStyles}
-            searchBox={this.searchBox}
-          />
-          {/* <Animated.View style={[styles.header, animatedStyle]}> */}
-          {/*  <Text>Most frequent</Text> */}
-          {/* <View style={styles.mostFrequentBody}> */}
-          {/*  {this.renderMostFrequentContactsBody({limit:3)} */}
-          {/* </View> */}
-          {/* <View style={styles.contactBody}> */}
-          {/*  {this.renderContactsBody()} */}
-          {/* </View> */}
-          {/* </Animated.View> */}
-        </View>
-      </ScrollView>
+      <View style={styles.row}>
+        <SearchBox
+          placeholder="Search by username or alias"
+          style={searchBoxStyles}
+          searchBox={this.searchBox}
+        />
+        <ScrollView style={styles.container}>
+          <Animated.View style={[styles.header, animatedStyle]}>
+            <View style={styles.contactBody}>
+              {this.renderContactsBody()}
+            </View>
+          </Animated.View>
+        </ScrollView>
+      </View>
+
     );
   }
 }
