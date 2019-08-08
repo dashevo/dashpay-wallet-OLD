3ee7e30d29277f029ada84cd3082915e2fb2b4e1
Denis <st.denis@me.com>
2019-08-08 17:15:13 +0300

    feat(registration): suggest mocked profile for the demo

--- src/containers/RegistrationScreen/selector.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from 'reselect';
-import { accountSelector } from 'state/account/selectors';
-
-export default createSelector(
-  accountSelector,
-  ({ username }) => ({ username }),
-);
